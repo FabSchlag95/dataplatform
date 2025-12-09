6b0a25cbdfdf02550528231da9ad19050c154536
FabSchlag95 <fabianschlagheck@mail.de>
2025-12-09 13:00:23 +0100

    .

--- volumes/db/mlflow.sql
+++ volumes/db/mlflow.sql
@@ -1,10 +1,13 @@
 CREATE SCHEMA IF NOT EXISTS mlflow AUTHORIZATION supabase_admin;
 GRANT USAGE, CREATE ON SCHEMA mlflow TO supabase_admin;
 
-\set pguser `echo "$POSTGRES_USER"`
 
 CREATE SCHEMA IF NOT EXISTS creator AUTHORIZATION supabase_admin;
 
--- allow the backend/client role to see and use the schema
-GRANT USAGE ON SCHEMA creator TO :pguser;
-GRANT CREATE ON SCHEMA creator TO :pguser;
+GRANT USAGE ON SCHEMA creator TO anon, authenticated, service_role;
+GRANT ALL ON ALL TABLES IN SCHEMA creator TO anon, authenticated, service_role;
+GRANT ALL ON ALL ROUTINES IN SCHEMA creator TO anon, authenticated, service_role;
+GRANT ALL ON ALL SEQUENCES IN SCHEMA creator TO anon, authenticated, service_role;
+ALTER DEFAULT PRIVILEGES FOR ROLE postgres IN SCHEMA creator GRANT ALL ON TABLES TO anon, authenticated, service_role;
+ALTER DEFAULT PRIVILEGES FOR ROLE postgres IN SCHEMA creator GRANT ALL ON ROUTINES TO anon, authenticated, service_role;
+ALTER DEFAULT PRIVILEGES FOR ROLE postgres IN SCHEMA creator GRANT ALL ON SEQUENCES TO anon, authenticated, service_role;

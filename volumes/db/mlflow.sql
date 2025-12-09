10d1b13ff1fbefaaa4bfd9c35906d06ec786a8aa
FabSchlag95 <fabianschlagheck@mail.de>
2025-12-09 12:50:33 +0100

    .

--- volumes/db/mlflow.sql
+++ volumes/db/mlflow.sql
@@ -1,2 +1,9 @@
 CREATE SCHEMA IF NOT EXISTS mlflow AUTHORIZATION supabase_admin;
 GRANT USAGE, CREATE ON SCHEMA mlflow TO supabase_admin;
+
+
+CREATE SCHEMA IF NOT EXISTS creator AUTHORIZATION supabase_admin;
+
+-- allow the backend/client role to see and use the schema
+GRANT USAGE ON SCHEMA creator TO authenticated;
+GRANT CREATE ON SCHEMA creator TO authenticated;
--- volumes/db/scalec_init.sql
+++
@@ -1,5 +0,0 @@
-CREATE SCHEMA IF NOT EXISTS creator AUTHORIZATION supabase_admin;
-
--- allow the backend/client role to see and use the schema
-GRANT USAGE ON SCHEMA creator TO authenticated;
-GRANT CREATE ON SCHEMA creator TO authenticated;

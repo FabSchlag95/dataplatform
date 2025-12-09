da8eb372ff2527ec653a35ad7bf90c08298a51c3
FabSchlag95 <fabianschlagheck@mail.de>
2025-12-09 12:43:00 +0100

    .

--- volumes/db/llm.sql
+++
@@ -1,5 +0,0 @@
-CREATE SCHEMA IF NOT EXISTS llm AUTHORIZATION supabase_admin;
-
--- allow the backend/client role to see and use the schema
-GRANT USAGE ON SCHEMA llm TO authenticated;
-GRANT CREATE ON SCHEMA llm TO authenticated;
--- volumes/db/playground.sql
+++
@@ -1,5 +0,0 @@
-CREATE SCHEMA IF NOT EXISTS playground AUTHORIZATION supabase_admin;
-
--- allow the backend/client role to see and use the schema
-GRANT USAGE ON SCHEMA playground TO authenticated;
-GRANT CREATE ON SCHEMA playground TO authenticated;

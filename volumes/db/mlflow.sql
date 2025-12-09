83a2b296fcb396a444d50204e20ff3341303bd5a
FabSchlag95 <fabianschlagheck@mail.de>
2025-12-09 12:56:03 +0100

    .

--- volumes/db/mlflow.sql
+++ volumes/db/mlflow.sql
@@ -1,9 +1,10 @@
 CREATE SCHEMA IF NOT EXISTS mlflow AUTHORIZATION supabase_admin;
 GRANT USAGE, CREATE ON SCHEMA mlflow TO supabase_admin;
 
+\set pguser `echo "$POSTGRES_USER"`
 
 CREATE SCHEMA IF NOT EXISTS creator AUTHORIZATION supabase_admin;
 
 -- allow the backend/client role to see and use the schema
-GRANT USAGE ON SCHEMA creator TO authenticated;
-GRANT CREATE ON SCHEMA creator TO authenticated;
+GRANT USAGE ON SCHEMA creator TO :pguser;
+GRANT CREATE ON SCHEMA creator TO :pguser;
